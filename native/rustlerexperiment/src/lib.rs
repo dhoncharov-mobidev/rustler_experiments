eb4694433988fda450fa6c620888e97010c4a3ec
Dmytro Honcharov <d.goncharov@mobidev.biz>
2022-11-04 01:46:04 +0400

    cleaned lib.rs, updated parser

--- native/rustlerexperiment/src/lib.rs
+++ native/rustlerexperiment/src/lib.rs
@@ -1,8 +1,7 @@
 use bgpkit_parser::BgpkitParser;
-use rustler::{Env, ListIterator, MapIterator, NifResult, Term};
-use serde_json::{Result, Value};
-use serde_rustler::{from_term, to_term};
-use std::collections::{hash_map, HashMap};
+use rustler::{Env, MapIterator, NifResult, Term};
+use serde_json::Value;
+use std::collections::HashMap;
 
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
@@ -22,35 +21,23 @@ fn mrt_parser<'a>(env: Env<'a>, path: String, filters: MapIterator<'a>) -> NifRe
             .unwrap()
     });
 
-    // let elements = BgpkitParser::new(&path).unwrap();
     for elem in elements {
         let mut map = Term::map_new(env);
         let str = serde_json::to_string(&elem).unwrap();
         let hm: HashMap<String, Value> = serde_json::from_str(&str).unwrap();
         map = Term::map_put(map, "timestamp", hm.get("timestamp").unwrap().as_f64()).unwrap();
         map = Term::map_put(map, "type", hm.get("type").unwrap().as_str()).unwrap();
+        map = Term::map_put(map, "peer_asn", hm.get("peer_asn").unwrap().as_i64()).unwrap();
+        map = Term::map_put(map, "peer_ip", hm.get("peer_ip").unwrap().as_str()).unwrap();
+        map = Term::map_put(map, "prefix", hm.get("prefix").unwrap().as_str()).unwrap();
+        map = Term::map_put(map, "next_hop", hm.get("next_hop").unwrap().as_str()).unwrap();
+        map = Term::map_put(map, "as_path", hm.get("as_path").unwrap().as_str()).unwrap();
+        map = Term::map_put(map, "origin_asns", hm.get("origin_asns").unwrap().as_str()).unwrap();
+        map = Term::map_put(map, "origin", hm.get("origin").unwrap().as_str()).unwrap();
         map = Term::map_put(map, "local_pref", hm.get("local_pref").unwrap().as_i64()).unwrap();
         map = Term::map_put(map, "med", hm.get("med").unwrap().as_i64()).unwrap();
-        map = Term::map_put(map, "peer_asn", hm.get("peer_asn").unwrap().as_i64()).unwrap();
-
         list = Term::list_prepend(list, map)
     }
-    // TODO: serde_rustler::Serializer raises next error, need to fix it
-    // the trait bound `serde_rustler::Serializer<'_>: From<Env<'a>>` is not satisfied
-    // the trait `From<rustler::env::Env<'a>>` is implemented for `serde_rustler::Serializer<'a>`
-    // for elem in BgpkitParser::new(&path).unwrap() {
-    //     let str = serde_json::to_string(&elem).unwrap();
-    //     serde_transcode::transcode(
-    //         &mut serde_json::Deserializer::from_str(&str),
-    //         serde_rustler::Serializer::from(env),
-    //     )
-    // .map_err(|e| e.into());
-    // let hm: HashMap<String, Value> = serde_json::from_str(&str).unwrap();
-    // for (k, v) in hm.iter() {
-    //     map = Term::map_put(map, k, v.to_string()).unwrap();
-    // }
-    // list = Term::list_prepend(list, map);
-    // }
     Ok(list)
 }
 
